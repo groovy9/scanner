78a3cc6daa25065c2e4b13aadc3c4d988ccf4a6c
groovy9 <jason@jnj.org>
2024-02-08 17:19:35 -0600

    .

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo apt -y update
 sudo apt -y upgrade
-sudo apt -y install pdftk imagemagick python2 parallel sox libleptonica-dev
+sudo apt -y install pdftk imagemagick python2 parallel sox libleptonica-dev evince sane-tools
 
 cd
 mkdir bin 2>/dev/null
@@ -10,14 +10,22 @@ git clone https://github.com/groovy9/scanner
 cd scanner
 sudo cp -f 60-libsane.rules /etc/udev/rules.d
 cd ~/bin
-ln -s ~/scanner/bin/scanner.sh 
+ln -s ~/scanner/bin/scanner.sh  scanner
+sudo usermod -G scanner `whoami`
 
 echo
 echo
 echo "Linux will now shut itself down.  Afterward, start it back up with the 'Ubuntu' application in the Windows start menu."
 echo
 echo
-
-echo -n "Press enter to continue"
-read foo
-cmd.exe /C wsl --shutdown
+echo 5
+sleep 1
+echo 4
+sleep 1
+echo 3
+sleep 1
+echo 2
+sleep 1
+echo 1
+sleep 1
+cmd.exe /C wsl --shutdown 
