7efc38ab61244b137e18d349c3b08003d7f6dec8
groovy9 <jason@jnj.org>
2025-01-27 16:09:13 -0600

    .

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo apt -y update
 sudo apt -y upgrade
-sudo apt -y install pdftk imagemagick python2 parallel sox libleptonica-dev evince sane-utils wlsu
+sudo apt -y install pdftk imagemagick python2 parallel sox libleptonica-dev evince sane-utils wslu
 
 cd
 mkdir bin 2>/dev/null
